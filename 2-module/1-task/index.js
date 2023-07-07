7cca1bafbaa11679c8c8fe0d1488e8889c243d1d
Ilya-Sakuro <114528785+Ilya-Sakuro@users.noreply.github.com>
2023-07-07 15:14:34 +0300

    Did 2-module

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,13 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary(obj) {
+  let result = 0
+
+  for (const key in obj) {
+    if (isFinite(obj[key])) {
+      result += obj[key]
+    }
+    if (!isFinite(obj[key])) {
+      break
+    }
+  }
+  return result
 }
