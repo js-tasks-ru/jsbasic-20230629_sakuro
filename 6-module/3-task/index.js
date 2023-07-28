7cc2845c09e3b7359278010bc669b41686943c0f
Ilya-Sakuro <114528785+Ilya-Sakuro@users.noreply.github.com>
2023-07-28 17:13:00 +0300

    did: 6-module/1-3 tasks

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,93 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class Carousel {
+  elem = null;
   constructor(slides) {
     this.slides = slides;
+    this.elem = this.#render();
   }
+  #template() {
+    return `
+    <div class="carousel">
+    <div class="carousel__arrow carousel__arrow_right">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon" />
+    </div>
+    <div class="carousel__arrow carousel__arrow_left">
+      <img src="/assets/images/icons/angle-left-icon.svg" alt="icon" />
+    </div>
+    <div class="carousel__inner">
+    ${this.slides
+      .map(
+        (elem) => `<div class="carousel__slide" data-id="${elem.id}">
+        <img src="/assets/images/carousel/${elem.image}"
+          class="carousel__img"
+          alt="slide"/>
+        <div class="carousel__caption">
+          <span class="carousel__price">€${elem.price.toFixed(2)}</span>
+          <div class="carousel__title">${elem.name}</div>
+          <button type="button" class="carousel__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon"/>
+          </button>
+        </div>
+      </div>`
+      )
+      .join("")}
+    </div>
+  </div>`;
+  }
+
+  #render() {
+    this.elem = createElement(this.#template());
+
+    const carouselButton = this.elem.querySelectorAll(".carousel__button");
+    carouselButton.forEach((button) => {
+      button.addEventListener("click", this.#onMenuClick);
+    });
+
+    const inner = this.elem.querySelector(".carousel__inner");
+    const carouselslides = this.elem.querySelectorAll(".carousel__slide");
+    const slideWidth = 500;
+    const leftButton = this.elem.querySelector(".carousel__arrow_left");
+    const rightButton = this.elem.querySelector(".carousel__arrow_right");
+    let currentIndex = 0;
+
+    leftButton.addEventListener("click", () => {
+      if (currentIndex > 0) {
+        currentIndex--;
+        inner.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+        rightButton.style.display = "";
+        if (currentIndex === 0) {
+          leftButton.style.display = "none";
+        }
+      }
+    });
+
+    rightButton.addEventListener("click", () => {
+      if (currentIndex < carouselslides.length - 1) {
+        currentIndex++;
+        inner.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+        leftButton.style.display = "";
+        if (currentIndex === carouselslides.length - 1) {
+          rightButton.style.display = "none";
+        }
+      }
+    });
+
+    leftButton.style.display = "none";
+
+    return this.elem;
+  }
+
+  #onMenuClick = (event) => {
+    const slide = event.target.closest(".carousel__slide");
+    if (slide) {
+      const id = slide.dataset.id;
+      console.log(id);
+      const event = new CustomEvent("product-add", {
+        bubbles: true,
+        detail: id,
+      });
+      this.elem.dispatchEvent(event);
+    }
+  };
 }
